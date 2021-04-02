19c493ef5d7aa198e473e67be3c9778b3ed7f9f7
insert-username <insert-username@users.noreply.github.com>
2021-04-02 22:19:16 +0200

    WS mirror server.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const ws = require('ws')
+
 console.log("Robit controller.")
+
+const server = new ws.Server({
+    port: 8080,
+});
+
+server.on('connection', function connection(socket) {
+    console.log("connection opened");
+
+    socket.on('message', function incoming(message) {
+        console.log("Incoming message: " + message);
+        socket.send(message); // mirror response
+    });
+});
+
+
