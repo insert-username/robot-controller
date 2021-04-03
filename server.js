ba37ebde3bba497454c81290337d7e0cf6e84b3b
insert-username <insert-username@users.noreply.github.com>
2021-04-03 14:37:29 +0200

    KISS: allow multiple server connections and just broadcast between clients.

--- server.js
+++ server.js
@@ -1,57 +1,22 @@
 const http = require('http')
 const ws = require('ws')
 
-var startServer = function(port, onConnectMade, onReceiveMsg) {
-    // assume single connection for simplicity's sake
-    var hasAllocated = false;
+// listen on single port,
+// for each connection, take input and broadcast to all clients.
+// ugly, but it works
 
-    const httpServer = http.createServer();
+const port = process.env.PORT || 8080;
 
-    const wsServer = new ws.Server({
-        noServer: true,
+const wsServer = new ws.Server({ port: port });
+
+wsServer.on('connection', function connection(socket) {
+    socket.on('message', data => {
+        wsServer.clients
+            .forEach(client => {
+                if (client != socket) {
+                    client.send(data);
+                }
+            });
     });
+});
 
-    wsServer.on('connection', function connection(socket) {
-        onConnectMade(socket);
-        socket.on('message', onReceiveMsg);
-    });
-
-    httpServer.on('upgrade', function upgrade(request, socket, head) {
-        if (hasAllocated) {
-          socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-          socket.destroy();
-          return;
-        }
-
-        hasAllocated = true;
-
-        wsServer.handleUpgrade(request, socket, head, function done(webSocket) {
-          wsServer.emit('connection', webSocket, request);
-        });
-    });
-
-    httpServer.listen(port);
-};
-
-
-var droneSocketHolder = {
-    socket: false
-};
-
-var droneConnection = startServer(
-    8081,
-    function(socket) {
-        droneSocketHolder.socket = socket
-    },
-    function(msg) { });
-
-var clientConnection = startServer(
-    8080,
-    function(socket) {},
-    function(msg) {
-        if (droneSocketHolder.socket) {
-            droneSocketHolder.socket.send(msg);
-        } else {
-            console.log("Discarding msg to drone: " + msg);
-        }
-    });
