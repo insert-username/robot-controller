d045275dedb652f814a95f6b0bbd02383942cb80
insert-username <insert-username@users.noreply.github.com>
2021-04-10 01:04:11 +0200

    Added http serving

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const http = require('http')
-const ws = require('ws')
+const http = require('http');
+const ws = require('ws');
+const express = require('express');
 
-// listen on single port,
-// for each connection, take input and broadcast to all clients.
-// ugly, but it works
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-const wsServer = new ws.Server({ port: port });
+
+const app = express();
+const expressServer = app.listen(PORT);
+app.use(express.static(__dirname + '/public'));
+const wsServer = new ws.Server({ server: expressServer });
 
 wsServer.on('connection', function connection(socket) {
     socket.on('message', data => {
